8c53954a52bb6aeb74f4c46cedd1718ae0a76851
Matt Beaudoin <ehxtwo@gmail.com>
2021-05-09 14:58:07 -0700

    adding signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,29 @@
 class UsersController < ApplicationController
 
-    
+    def new
+        @user = User.new
+    end
+
+    def create
+        @user = User.new(user_params)
+
+        if @user.save
+            redirect_to action: 'index'
+        else
+            redirect_to action: 'new'
+        end
+
+    end
+
+    def index
+
+    end
+
+
+    private
+
+        def user_params
+            params.require(:user).permit(:password, :email)
+        end
 
 end
