2ee584a88ab5df56d1eee0881c0276e405f2f119
Matt Beaudoin <ehxtwo@gmail.com>
2021-05-10 13:57:23 -0700

    login form

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,15 +5,22 @@ class SessionsController < ApplicationController
     end
     def create
         @user = User.find_by(email: params[:email])
-        session[:current_user_id] = @user.id
+        if @user != nil
+            binding.pry
+            session[:current_user_id] = @user.id
 
-        if  @user && @user.authenticate(params[:password])
-            session[:user_id] = @user.id
+            if  @user && @user.authenticate(params[:password])
+                session[:user_id] = @user.id
 
-          redirect_to user_path(@user)
-      else
-          redirect_to login_path
-      end
+                redirect_to user_path(@user)
+            else
+                redirect_to login_path
+            end
+        else
+            @user= User.new
+            @user.errors << "unknown email"
+            redirect_to login_path
+        end
     end
 
     def index
